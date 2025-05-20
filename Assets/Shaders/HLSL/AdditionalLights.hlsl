db72c511d912f308ea0729fb9e7f534bc11ac9cb
jeremythedrawer <jedwards184@outlook.com>
2025-05-21 00:15:55 +1000

    tweeked shader

--- Assets/Shaders/HLSL/AdditionalLights.hlsl
+++ Assets/Shaders/HLSL/AdditionalLights.hlsl
@@ -1,8 +1,10 @@
-﻿void ComputeTotalAttenuation_half(float3 WorldPosition, out float TotalAttenuation, out float3 LightColor)
+﻿void ComputeTotalAttenuation_half(float3 WorldPosition, float3 WorldNormal, out float TotalAttenuation, out float3 LightColor)
 {
+
+
     TotalAttenuation = 0.0;
     LightColor = float3(0, 0, 0);
-    
+
 #ifndef SHADERGRAPH_PREVIEW
 
     uint pixelLightCount = GetAdditionalLightsCount();
@@ -10,13 +12,18 @@
     LIGHT_LOOP_BEGIN(pixelLightCount)
 
     uint perObjectLightIndex = GetPerObjectLightIndex(lightIndex);
-
     Light light = GetAdditionalPerObjectLight(perObjectLightIndex, WorldPosition);
 
     float atten = light.distanceAttenuation;
+
+    float NdotL = saturate(dot(WorldNormal, light.direction));
+
+    float diffuse = atten * NdotL;
+
     float intensity = dot(light.color.rgb, float3(0.2126, 0.7152, 0.0722));
     TotalAttenuation += atten * intensity;
     LightColor += light.color;
+
     LIGHT_LOOP_END
 
 #endif
